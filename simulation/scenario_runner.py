3692f2a0c21bcec582fe98a9c1637f9c6cbe960c
Gustavo <gmateos.aws@gmail.com>
2026-03-15 15:36:38 +0100

    Defect fixes scenario_runner redundant code

--- simulation/scenario_runner.py
+++ simulation/scenario_runner.py
@@ -2,9 +2,4 @@ from simulation.montecarlo import monte_carlo
 
 
 def run_scenario(system_model, price, marketing):
-    result = monte_carlo(system_model, price, marketing)
-
-    result["price"] = price
-    result["marketing"] = marketing
-
-    return result
+    return monte_carlo(system_model, price, marketing)
